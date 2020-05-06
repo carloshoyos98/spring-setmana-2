a5a7b2eff1f13cfebc4e151504701e2be7a49eae
carloshoyos98 <choyos@cifpfbmoll.eu>
2020-05-06 15:01:45 +0200

    feat: Añadir método afegir

--- src/main/java/org/formacio/mvc/MockController.java
+++ src/main/java/org/formacio/mvc/MockController.java
@@ -3,7 +3,9 @@ package org.formacio.mvc;
 import org.formacio.repositori.AgendaService;
 import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,9 +33,26 @@ public class MockController {
 	
 	@RequestMapping(path = "/contacte/{id}", produces= {MediaType.APPLICATION_JSON_VALUE,
 														MediaType.APPLICATION_XML_VALUE})
+
+	public ResponseEntity<Persona> contacte(@PathVariable String id ) {
+		if (!servei_agenda.getBD().containsKey(id)) {
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<>(servei_agenda.recupera(id), HttpStatus.OK);
+			
+	}
+	
+	@RequestMapping(path = "/afegir", method = RequestMethod.POST)
 	@ResponseBody
-	public Persona contacte(@PathVariable String id ) {
-		return servei_agenda.recupera(id);
+	public void afegir(@RequestParam(name="id",
+									 required=true) String id,
+					   @RequestParam(name="nom",
+							   		 required=true) String nom,
+					   @RequestParam(name="telefon",
+							   		 required=true) String telefon) {
+		servei_agenda.inserta(id, nom, telefon);
+		
 	}
 }
 
